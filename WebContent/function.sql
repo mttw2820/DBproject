cf626a1fb3cef1b8b42c8fc85b3d0b4259717777
hipppoSHL <shjh1114@icloud.com>
2020-06-20 01:28:33 +0900

    Update function.sql

--- WebContent/function.sql
+++ WebContent/function.sql
@@ -4,7 +4,7 @@ IS
   nYear       NUMBER;
   sMonth      VARCHAR2(2);
 BEGIN
-  /* 11¿ù ~ 4¿ù : 1ÇÐ±â, 5¿ù ~ 10¿ù : 2ÇÐ±â */
+  /* 11ì›” ~ 4ì›” : 1í•™ê¸°, 5ì›” ~ 10ì›” : 2í•™ê¸° */
   SELECT TO_NUMBER(TO_CHAR(in_date, 'YYYY')), TO_CHAR(in_date, 'MM') 
   INTO nYear, sMonth 
   FROM DUAL;
@@ -23,7 +23,7 @@ IS
   nSemester   NUMBER;
   sMonth      VARCHAR2(2);
 BEGIN
-  /* 11¿ù ~ 4¿ù : 1ÇÐ±â, 5¿ù ~ 10¿ù : 2ÇÐ±â */
+  /* 11ì›” ~ 4ì›” : 1í•™ê¸°, 5ì›” ~ 10ì›” : 2í•™ê¸° */
   SELECT TO_CHAR(in_date, 'MM') 
   INTO sMonth 
   FROM DUAL;
@@ -36,4 +36,69 @@ BEGIN
   
   RETURN nSemester;
  END;
+/
+                           
+```sql
+CREATE OR REPLACE FUNCTION Date2EnrollYear(in_date IN DATE)
+RETURN NUMBER
+IS
+  nYear       NUMBER;
+  sMonth      VARCHAR2(2);
+BEGIN
+  /* 11ì›” ~ 4ì›” : 1í•™ê¸°, 5ì›” ~ 10ì›” : 2í•™ê¸° */
+  SELECT TO_NUMBER(TO_CHAR(in_date, 'YYYY')), TO_CHAR(in_date, 'MM') 
+  INTO nYear, sMonth 
+  FROM DUAL;
+
+  IF (sMonth ='11' or sMonth='12') THEN
+     nYear := nYear + 1;
+  END IF; 
+  
+  RETURN nYear;
+END;
+/
+
+CREATE OR REPLACE FUNCTION Date2EnrollSemester(in_date IN DATE)
+RETURN NUMBER
+IS
+  nSemester   NUMBER;
+  sMonth      VARCHAR2(2);
+BEGIN
+  /* 11ì›” ~ 4ì›” : 1í•™ê¸°, 5ì›” ~ 10ì›” : 2í•™ê¸° */
+  SELECT TO_CHAR(in_date, 'MM') 
+  INTO sMonth 
+  FROM DUAL;
+
+  IF ( (sMonth='11' or sMonth='12') or (sMonth >='01' and sMonth <= '04') ) THEN 
+      nSemester := 1;
+  ELSE
+      nSemester := 2;
+  END IF;
+  
+  RETURN nSemester;
+ END;
+/
+
+CREATE OR REPLACE FUNCTION day_map(day_num IN NUMBER)
+RETURN VARCHAR2
+IS
+BEGIN
+	IF (day_num = 1) THEN
+		RETURN 'ì›”';
+	ELSIF (day_num = 2) THEN
+		RETURN 'í™”';
+	ELSIF (day_num = 3) THEN
+		RETURN 'ìˆ˜';
+	ELSIF (day_num = 4) THEN
+		RETURN 'ëª©';
+	ELSIF (day_num = 5) THEN
+		RETURN 'ê¸ˆ';
+	ELSIF (day_num = 6) THEN
+		RETURN 'í† ';
+	ELSIF (day_num = 7) THEN
+		RETURN 'ì¼';
+	ELSE	
+		RETURN '';
+	END IF;
+END;
 /
